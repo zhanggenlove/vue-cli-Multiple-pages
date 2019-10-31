eb965ded5ffb232e1caa146f1e12f17c39d9c981
1519149300@qq.com <1519149300@qq.com>
2019-10-31 17:31:57 +0800

    demo

---
+++ src/pages/home/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div id="nav">
+		我是home页面啊啊啊啊 啊 
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+</style>
